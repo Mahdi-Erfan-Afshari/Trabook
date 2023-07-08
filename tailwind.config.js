5196904637c5d5fa2a409dee0f75e094dfee5d57
Mahdi-Erfan-Afshari <mahdiafshar2413@gmail.com>
2023-07-08 16:54:17 +0330

    Update Project

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+
+    extend: {
+      boxShadow: {
+        '3xl' : '0px 0px 20px 2px #eee'
+      },
+      
+      colors: {
+        Orange: '#FA7436',
+        LightOrange: '#FFBFA2',
+        BrightOrange: '#ff8a40',
+        BrightSupOrange: '#ff9d8d',
+        BrightSupLightOrange: '#ff793b6c',
+        SupLightOrange: '#ffe8dd',
+        Blue: '#4086F4',
+        Black: '#222',
+        LightBlack: '#444',
+        BrightBlack: '#666',
+        Gray: '#999',
+        LightGray: '#E5E5E5',
+        BrightGraySupLight: '#F7F8Fc',
+        White: '#FEFCFB',
+        Yellow: '#FFB60A'
+      }
+    },
+  },
+  plugins: [],
+}
